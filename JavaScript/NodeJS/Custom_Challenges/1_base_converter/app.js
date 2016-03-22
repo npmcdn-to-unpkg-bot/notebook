93d7ed6a572778f97110aee3414695f335670695
l4nk332 <l4nk332@gmail.com>
2016-03-21 19:28:34 -0700

    Sets up first custom challenge with Unit Tests and package.json

---
+++ JavaScript/NodeJS/Custom_Challenges/1_base_converter/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function binaryToDecimal(binValue) {
+  if (/[01]/.test(binValue) === true) {
+    // Use parseInt to convert binary (base2) to number
+    let decimal = Number.parseInt(binValue, 2);
+    return decimal;
+  } else {
+  // Handle errors here...
+  }
+}
+
+module.exports = {
+  binaryToDecimal: binaryToDecimal
+};
---
+++ JavaScript/NodeJS/Custom_Challenges/1_base_converter/test/app-test.js
@@ -0,0 +1,27 @@
+var expect = require("chai").expect;
+var app = require("../app");
+
+describe("binaryToDecimal()", function() {
+
+  it("should convert simple binary to decimal value", function() {
+    expect(app.binaryToDecimal("0")).to.equal(0);
+    expect(app.binaryToDecimal("1")).to.equal(1);
+    expect(app.binaryToDecimal("101")).to.equal(5);
+    expect(app.binaryToDecimal("1000")).to.equal(8);
+    expect(app.binaryToDecimal("1111")).to.equal(15);
+    expect(app.binaryToDecimal("101011")).to.equal(43);
+  });
+
+  it("should convert large binary to decimal value", function () {
+    expect(app.binaryToDecimal("10000000")).to.equal(128);
+    expect(app.binaryToDecimal("10111101110110")).to.equal(12150);
+    expect(app.binaryToDecimal("11111111111111111111")).to.equal(1048575);
+    expect(app.binaryToDecimal("111010101011110111000")).to.equal(1923000);
+    expect(app.binaryToDecimal("10000101010110111111000000110")).to.equal(279674374);
+    expect(app.binaryToDecimal("111100001101010110111111000111000110")).to.equal(64648638918);
+  });
+
+  it("should throw an error for invalid entries", function() {
+  
+  });
+});
