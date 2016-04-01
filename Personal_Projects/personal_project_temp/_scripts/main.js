d739fbe6adf4685e22bc7fb2f385b68d2c272aac
l4nk332 <l4nk332@gmail.com>
2016-03-31 23:53:29 -0700

    Optimizes image assets. Adds slight zoom on :hover of each svg panel. Adds media query styles for mobile layout (both pages). Add 'background-attachment: fixed' on background image on page 2. Adds content fade-in on scroll

--- Personal_Projects/personal_project_temp/_scripts/main.js
+++ Personal_Projects/personal_project_temp/_scripts/main.js
@@ -10,3 +10,20 @@ $(document).ready(function() {
     });
   });
 });
+
+// Add onload for scroll-fade effect
+$(window).on("load", function () {
+  $(window).scroll(function() {
+    $(".row").each(function() {
+      var columnA = $(this).children(".col-1");
+      var columnB = $(this).children(".col-2");
+      var rowBottom = $(this).offset().top + ($(this).outerHeight() / 2);
+      var scrollBottom = $(window).scrollTop() + $(window).innerHeight();
+
+      if (rowBottom < scrollBottom) {
+        $(columnA).fadeTo(500, 1);
+        $(columnB).fadeTo(500, 1);
+      }
+    });
+  });
+});
