48da21bac3fe4e9d1110f849459b806eed7a35dd
l4nk332 <l4nk332@gmail.com>
2016-04-02 17:25:39 -0700

    Adds gif modal with styles. Changes first row in page 2 to full opacity at start. Adds favicons for both pages of site

--- Personal_Projects/personal_project/_scripts/main.js
+++ Personal_Projects/personal_project/_scripts/main.js
@@ -5,6 +5,22 @@ $(document).ready(function() {
   $(".container").on("click", ".panel", function() {
     $(this).toggleClass("flipped");
   });
+  // Create and Attach Modal Elmt
+  $("h1").on("click", function(e) {
+    if (e.shiftKey) {
+      var wrapper = document.createElement("div");
+      $(wrapper).addClass("modal-wrapper");
+
+      $(wrapper).append("<img src='assets/images/excited.gif' alt='Bill Murray celebration' class='giphy-embed'><button id='exit'>Close</button>");
+
+      $("body").append(wrapper);
+      // Remove the elmt from DOM
+      $("#exit").on("click", function() {
+        $(wrapper).remove();
+      });
+    }
+  });
+
 });
 
 // Add onload for scroll-fade effect
