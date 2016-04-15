a7ab3fbf765f0eb8fad6e58e34e45b113a022e57
l4nk332 <l4nk332@gmail.com>
2016-04-15 15:11:33 -0700

    Completes Basic Site project initialization. Adds new node dependency to find user home directory.

--- JavaScript/NodeJS/Custom_Challenges/3_project_initializer/app.js
+++ JavaScript/NodeJS/Custom_Challenges/3_project_initializer/app.js
@@ -1,5 +1,8 @@
 var prompt = require("prompt");
 var colors = require("colors/safe");
+var homedir = require("homedir");
+// Custom modules
+var basic = require("./lib/basic");
 
 // Customize prompt
 prompt.message = "";
@@ -10,8 +13,67 @@ prompt.start();
 
 var selection = {};
 
+var selectionRoute = function() {
+  if (selection.type === '1') {
+    basic.setup(selection);
+  } else if (selection.type === '2') {
+    console.log("Move to App Setup");
+  }
+};
+
+var defaultFileNames = [
+  "tilemvcde",
+  "alider",
+  "Milkshapp",
+  "machenejs",
+  "Overgeo",
+  "Ancience",
+  "qwesom",
+  "Rumbars",
+  "Codelor",
+  "spathquire",
+  "Leafclude",
+  "jpars",
+  "wormie",
+  "unifoxjs",
+  "glader",
+  "ingode",
+  "clepotr",
+  "knocketche",
+  "synturid",
+  "libraerge"
+];
+
 function init() {
   prompt.addProperties(selection, [{
+    name: "name",
+    description: colors.white("Project Name: "),
+    message: `Please enter a project name`,
+    required: true,
+    type: 'string',
+    default: defaultFileNames[Math.floor(Math.random()*defaultFileNames.length)],
+    before: function(value) {
+      if (value === "exit") {
+        process.exit();
+      } else {
+        return value;
+      }
+    }
+  }, {
+    name: "path",
+    description: colors.white("Project Root Path: "),
+    message: `Please enter a valid path`,
+    required: true,
+    default: `${homedir()}/Desktop`,
+    pattern: /^\/?(?:[^\/\s]\/)?(?:[^\/\s]+\/?)*$/,
+    before: function(value) {
+      if (value === "exit") {
+        process.exit();
+      } else {
+        return value;
+      }
+    }
+  }, {
     name: "type",
     description: colors.white("Project Type? ('1' for Basic Web / '2' for Web App) "),
     message: `Please select 1' or '2'`,
@@ -93,11 +155,13 @@ function verify() {
   prompt.get([{
     name: "verify",
     description: colors.yellow(`
-Type: ${colors.cyan((selection.type === '1') ? "Basic Web" : "Web App")},
-Gitignore: ${colors.cyan((selection.gitignore === 'y') ? "Yes" : "No")},
-Readme: ${colors.cyan((selection.gitignore === 'y') ? "Yes" : "No")},
-Test: ${colors.cyan((selection.gitignore === 'y') ? "Yes" : "No")},
-Gruntfile: ${colors.cyan((selection.gitignore === 'y') ? "Yes" : "No")}
+Name: ${colors.cyan(selection.name)}
+Path: ${colors.cyan(selection.path + '/' + selection.name)}
+Type: ${colors.cyan((selection.type === '1') ? "Basic Web" : "Web App")}
+Gitignore: ${colors.cyan((selection.gitignore === 'y') ? "Yes" : "No")}
+Readme: ${colors.cyan((selection.readme === 'y') ? "Yes" : "No")}
+Test: ${colors.cyan((selection.test === 'y') ? "Yes" : "No")}
+Gruntfile: ${colors.cyan((selection.gruntfile === 'y') ? "Yes" : "No")}
 
 ${colors.white("Is this correct? ('y'/'n')")}`),
     message: "Please select 'y' or 'n'",
@@ -113,6 +177,6 @@ ${colors.white("Is this correct? ('y'/'n')")}`),
     }
   }], function(err, result) {
     if (err) { console.error(`There was an error: ${err}`); }
-    (result.verify === "y") ? console.log("continue") : init();
+    (result.verify === "y") ? selectionRoute() : init();
   });
 }
